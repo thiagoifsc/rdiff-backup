71f75f1d2d499d8d88fb674310df9a2b15ffc8e5
Thiago Cunha <thiago.ifsc@gmail.com>
2013-11-28 11:40:59 -0200

    Alterado local onde é lido os arquivos de configuração para o diretório onde o script esta sendo rodado

--- rdiff-backup.sh
+++ rdiff-backup.sh
@@ -2,24 +2,26 @@
 # Script de automatização do rdiff-backup
 # Thiago Felipe da Cunha
 
-# organização
-org=`cat rdiff-backup.conf|grep org=|cut -d\" -f2`
-# email para onde serão enviados os relatórios
-mail=`cat rdiff-backup.conf|grep mail=|cut -d\" -f2`
-# dias de manutencao do backup incremental
-dias=`cat rdiff-backup.conf|grep dias=|cut -d\" -f2`
-# habilita/desabilita o log
-log=`cat rdiff-backup.conf|grep log=|cut -d\= -f2`
-# pasta onde o backup deve ser salvo
-destino=`cat rdiff-backup.conf|grep destino=|cut -d\" -f2`
+local=`echo $0 | awk -F'/' '{for (i=1; i<NF; i++) printf("%s/", $i)}'`
 
-discos=`cat disklist.conf | sed '/^\( *$\| *#\)/d'| wc -l`
+# organização
+org=`cat $local/rdiff-backup.conf|grep org=|cut -d\" -f2`
+# email para onde serão enviados os relatórios
+mail=`cat $local/rdiff-backup.conf|grep mail=|cut -d\" -f2`
+# dias de manutencao do backup incremental
+dias=`cat $local/rdiff-backup.conf|grep dias=|cut -d\" -f2`
+# habilita/desabilita o log
+log=`cat $local/rdiff-backup.conf|grep log=|cut -d\= -f2`
+# pasta onde o backup deve ser salvo
+destino=`cat $local/rdiff-backup.conf|grep destino=|cut -d\" -f2`
+
+discos=`cat $local/disklist.conf | sed '/^\( *$\| *#\)/d'| wc -l`
 #c2=0
 for (( c=1; c <= $discos; c++ ))
 do
-	host[$c-1]=`cat disklist.conf | sed '/^\( *$\| *#\)/d'|sed ''$c'!d' |cut -d: -f1`
-        diretorios_backup[$c-1]=`cat disklist.conf | sed '/^\( *$\| *#\)/d'|sed ''$c'!d' |cut -d: -f2`
-	usuario[$c-1]=`cat disklist.conf | sed '/^\( *$\| *#\)/d'|sed ''$c'!d' |cut -d: -f4`
+	host[$c-1]=`cat $local/disklist.conf | sed '/^\( *$\| *#\)/d'|sed ''$c'!d' |cut -d: -f1`
+        diretorios_backup[$c-1]=`cat $local/disklist.conf | sed '/^\( *$\| *#\)/d'|sed ''$c'!d' |cut -d: -f2`
+	usuario[$c-1]=`cat $local/disklist.conf | sed '/^\( *$\| *#\)/d'|sed ''$c'!d' |cut -d: -f4`
 	diretorios_excluir[$c-1]=`cat disklist.conf | sed '/^\( *$\| *#\)/d'|sed ''$c'!d' |cut -d: -f3`
 	qtd_excluir[$c-1]=`echo ${diretorios_excluir[$c-1]} | sed 's/,/ /g' | wc -w`
 done
